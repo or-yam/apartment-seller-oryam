985fc8ddee1835c36e431f653316a3dbc08f5fa5
Oryam <48219965+or-yam@users.noreply.github.com>
2020-06-28 15:14:59 +0300

    handlebars added and parking filtter

--- main.js
+++ main.js
@@ -1,20 +1,37 @@
+$('button').on('click', function () {
+  let address = $('#addr-input').val();
+  let minPrice = $('#min-p-input').val();
+  let maxPrice = $('#max-p-input').val();
+  let minRooms = $('#min-r-input').val();
+  let maxRooms = $('#max-r-input').val();
+  let immediate = $('#immed-y');
+  let parking = $('#park-y');
 
-$("button").on("click", function () {
-    let address = $("#addr-input").val()
-    let minPrice = $("#min-p-input").val()
-    let maxPrice = $("#max-p-input").val()
-    let minRooms = $("#min-r-input").val()
-    let maxRooms = $("#max-r-input").val()
-    let immediate = $("#immed-y")
-
-    let relevantApts = findRelevantApts(address, minPrice, maxPrice, minRooms, maxRooms, immediate)
-    renderApts(relevantApts)
-})
+  let relevantApts = findRelevantApts(
+    address,
+    minPrice,
+    maxPrice,
+    minRooms,
+    maxRooms,
+    immediate,
+    parking
+  );
+  renderApts(relevantApts);
+});
 
 const renderApts = function (apartments) {
-    $("#results").empty()
-    console.log(apartments) //array of apartments to render
-    //Your code goes here.
-}
+  $('#results').empty();
+  console.log(apartments); //array of apartments to render
+  if (apartments.length === 0) {
+    const source = $('#empty-template').html();
+    const template = Handlebars.compile(source);
+    const templateHTML = template();
+    $('#results').append(templateHTML);
+  }
+  const source = $('#apt-template').html();
+  const template = Handlebars.compile(source);
+  const templateHTML = template({ apartments });
+  $('#results').append(templateHTML);
+};
 
-renderApts(apartments) //renders apartments when page loads
+renderApts(apartments); //renders apartments when page loads
